48e41e34517a0adc5ab0ad3d5682b35225dcd2f6
Seokbong <dfbgq346q@gmail.com>
2022-02-03 17:50:08 +0900

    close 그래프 추가, 그래프 관련 설정 추가

--- fin_func.py
+++ fin_func.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+
 # [ 함수 ]
 # 시장 데이터 정리
 def stockDataOrgan(stockData):
@@ -122,19 +124,19 @@ def exchangeDataOrgan(exchangeData):
     # Date[0] Exchange Rate[1]
     return data
 
-# 시간 좌표 설정
+# 시간 좌표 설정 (시장, 환율)
 def timelineSet(organStock, organExchange):
     # 날짜 기준 배열 생성
-    timeline = organStock[4][0]
+    timeline = organStock[3][0]
 
     # stocks
-    # 코스피 [0], 코스닥 [1], 나스닥 [2], 다우지수 [3], 스탠다드 푸어스 [4], 환율(Korea - USA) [5]
+    # 코스피 [0], 코스닥 [1], 다우존스 [2], S&P 500 [3], 나스닥	[4], 환율(Korea - USA) [5]
     # 환율(Korea - USA) : organExchange
     stocks = []
     for st in range(0, 6):
         print("st : ", st)
         stocks.append([])
-        for os in range(0, len(organStock[4][0])):
+        for os in range(0, len(timeline)):
             stocks[st].append(0)
 
     print("stocks ??", len(stocks))
@@ -143,35 +145,35 @@ def timelineSet(organStock, organExchange):
     for time in timeline:
         # kospi에 기록이 있는 값만
         if time in organStock[0][0]:
-            stocks[0][organStock[4][0].index(time)] = organStock[0][4][organStock[0][0].index(time)]
+            stocks[0][timeline.index(time)] = organStock[0][4][organStock[0][0].index(time)]
             # print("time : ", time, "\tindex : ", organStock[0][0].index(time), "\tvalue : ", organStock[0][4][organStock[0][0].index(time)])
         # kosdaq 기록이 있는 값만
         if time in organStock[1][0]:
-            stocks[1][organStock[4][0].index(time)] = organStock[1][4][organStock[1][0].index(time)]
+            stocks[1][timeline.index(time)] = organStock[1][4][organStock[1][0].index(time)]
             # print("time : ", time, "\tindex : ", organStock[1][0].index(time), "\tvalue : ", organStock[1][4][organStock[1][0].index(time)])
-        # nasdaq 기록이 있는 값만
-        if time in organStock[2][0]:
-            stocks[2][organStock[4][0].index(time)] = organStock[2][4][organStock[2][0].index(time)]
-            # print("time : ", time, "\tindex : ", organStock[2][0].index(time), "\tvalue : ", organStock[2][4][organStock[2][0].index(time)])
         # djia 기록이 있는 값만
-        if time in organStock[3][0]:
-            stocks[3][organStock[4][0].index(time)] = organStock[3][4][organStock[3][0].index(time)]
-            # print("time : ", time, "\tindex : ", organStock[3][0].index(time), "\tvalue : ", organStock[3][4][organStock[3][0].index(time)])
+        if time in organStock[2][0]:
+            stocks[2][timeline.index(time)] = organStock[2][4][organStock[2][0].index(time)]
+            # print("time : ", time, "\tindex : ", organStock[2][0].index(time), "\tvalue : ", organStock[2][4][organStock[2][0].index(time)])
         # sp 기록이 있는 값만
+        if time in organStock[3][0]:
+            stocks[3][timeline.index(time)] = organStock[3][4][organStock[3][0].index(time)]
+            # print("time : ", time, "\tindex : ", organStock[3][0].index(time), "\tvalue : ", organStock[3][4][organStock[3][0].index(time)])
+        # nasdaq 기록이 있는 값만
         if time in organStock[4][0]:
-            stocks[4][organStock[4][0].index(time)] = organStock[4][4][organStock[4][0].index(time)]
+            stocks[4][timeline.index(time)] = organStock[4][4][organStock[4][0].index(time)]
             # print("time : ", time, "\tindex : ", organStock[4][0].index(time), "\tvalue : ", organStock[4][4][organStock[4][0].index(time)])
         # 환율 기록이 있는 값만 
         if time in organExchange[0][0]:
-            stocks[5][organStock[4][0].index(time)] = organExchange[0][1][organExchange[0][0].index(time)]
+            stocks[5][timeline.index(time)] = organExchange[0][1][organExchange[0][0].index(time)]
             # print("time : ", time, "\tindex : ", organStock[4][0].index(time), "\tvalue : ", organExchange[0][1][organExchange[0][0].index(time)])
     
-    # print("kospi ?? : ", stocks[0])
-    # print("kosdaq ?? : ", stocks[1])
-    # print("nasdaq ?? : ", stocks[2])
-    # print("djia ?? : ", stocks[3])
-    # print("sp ?? : ", stocks[4])
-    # print("ku ?? : ", stocks[5])
+    print("kospi ?? : ", stocks[0][0])
+    print("kosdaq ?? : ", stocks[1][0])
+    print("djia ?? : ", stocks[2][0])
+    print("sp ?? : ", stocks[3][0])
+    print("nasdaq ?? : ", stocks[4][0])
+    print("ku ?? : ", stocks[5][0])
 
     # 0 값 변경
     for market in range(0, len(stocks)):
@@ -181,3 +183,38 @@ def timelineSet(organStock, organExchange):
 
     # close 정보 리턴
     return stocks
+
+# figure 설정 및 저장 (x축, y축[배열], label[배열], x축 값 간격, y축 값 표기 간격)
+def figureSet(timeline, markets, labels, difX, difY):
+    # 그래프
+    plt.figure(figsize=(48, 30))
+
+    # 그래프 그리기
+    for market, label in zip(markets, labels):
+        plt.plot(timeline, market, label=label)
+        plt.legend()
+
+        # difX 일 마다 지수 값 표시
+        for i in range(0, len(timeline), difX):
+            # x, y, 값 표기 위치, ...
+            if market[i] is not None and market[i-difX] is not None:
+                if i > 0:
+                        plt.text(timeline[i], market[i] + difY, round(market[i], 2), horizontalalignment='center', verticalalignment='bottom', size = 12)
+                        plt.plot(timeline[i], market[i], 'ro')
+                        
+        # 마지막 날 무조건 표기 (휴장 및 휴일)
+        for i in range(1, 10, 1):
+            if market[-i] is not None:
+                plt.text(timeline[-i], market[-i] + difY, round(market[-i], 2), horizontalalignment='center', verticalalignment='bottom', size = 12)
+                plt.plot(timeline[-i], market[-i], 'ro')
+                break
+
+    # 저장
+    if len(labels)>1:
+        label = ""
+        for text in labels:
+            label = label + "_" + text
+        plt.savefig(label+'.png')
+    else:    
+        plt.savefig(labels[0]+'.png')
+    plt.cla()
--- fin_graph.py
+++ fin_graph.py
@@ -8,28 +8,28 @@ wb = load_workbook(filename="fin.xlsx")
 # sheet??
 # print(wb.sheetnames)
 
-# data??
-kospi = wb['data']['A':'E']      # 코스피 [0]
-kosdaq = wb['data']['H':'L']     # 코스닥 [1]
-nasdaq = wb['data']['O':'S']     # 나스닥 [2]
-djia = wb['data']['V':'Z']       # 다우지수 [3]
-sp = wb['data']['AC':'AG']       # 스탠다드 푸어스 [4]
-ku = wb['data']['AL':'AM']       # 환율(Korea - USA) [5]
+# data?? 코스피, 코스닥, 다우존스, S&P 500, 나스닥	
+kospi = wb['data']['A':'E']         # 코스피 [0]
+kosdaq = wb['data']['H':'L']        # 코스닥 [1]
+djia = wb['data']['O':'S']          # 나스닥 [2]
+sp = wb['data']['V':'Z']            # 다우지수 [3]
+nasdaq = wb['data']['AC':'AG']      # 스탠다드 푸어스 [4]
+ku = wb['data']['AL':'AM']          # 환율(Korea - USA) [5]
 
 # 시장
 stocks = []
 stocks.append(kospi)
 stocks.append(kosdaq)
-stocks.append(nasdaq)
 stocks.append(djia)
 stocks.append(sp)
+stocks.append(nasdaq)
 
 # 데이터 가공
 organStock = []
 organExchange = []
 for stock in stocks:
     # 코스피 코스닥 나스닥 다우지수 스탠다드
-    # Date Open High Low Close
+    # Date[0] Open[1] High[2] Low[3] Close[4]
     organStock.append(fin_func.stockDataOrgan(stock))
 organExchange.append(fin_func.exchangeDataOrgan(ku))
 
@@ -39,24 +39,22 @@ timeSetData = fin_func.timelineSet(organStock, organExchange)
 # dimension
 # organ[stock(market)][stock column][data]
 
-timeline = organStock[4][0]
+timeline = organStock[3][0]
 kospiClose = timeSetData[0]
 kosdaqClose = timeSetData[1]
-nasdaqClose = timeSetData[2]
-djiaClose = timeSetData[3]
-spClose = timeSetData[4]
+djiaClose = timeSetData[2]
+spClose = timeSetData[3]
+nasdaqClose = timeSetData[4]
 kuClose = timeSetData[5]
 
 # 그래프
-# plt.figure(figsize=(12, 7))
-plt.figure(figsize=(48, 30))
-# scatter, s=int
-plt.plot(timeline, kospiClose, label='kospi')
-plt.plot(timeline, kosdaqClose, label='kosdaq')
-plt.plot(timeline, nasdaqClose, label='nasdaq')
-plt.plot(timeline, djiaClose, label='djia')
-plt.plot(timeline, spClose, label='sp')
-plt.plot(timeline, kuClose, label='ku')
-plt.legend()
-plt.savefig('graph_close.png')
-# plt.show()
+fin_func.figureSet(timeline, [kospiClose], ['kospiClose'], 50, 10)
+fin_func.figureSet(timeline, [kosdaqClose], ['kosdaqClose'], 50, 5)
+fin_func.figureSet(timeline, [djiaClose], ['djiaClose'], 50, 100)
+fin_func.figureSet(timeline, [spClose], ['spClose'], 50, 100)
+fin_func.figureSet(timeline, [nasdaqClose], ['nasdaqClose'], 50, 100)
+fin_func.figureSet(timeline, [kuClose], ['kuClose'], 50, 10)
+fin_func.figureSet(timeline, [kospiClose, kosdaqClose, spClose, kuClose], ['kospiClose', 'kosdaqClose', 'spClose', 'kuClose'], 50, 10)
+fin_func.figureSet(timeline, [djiaClose, nasdaqClose], ['djiaClose', 'nasdaqClose'], 50, 100)
+
+
